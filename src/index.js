17726a4538fc629f5aae779a2708b0ff39ad9ab1
Daniil Parkov <damnwnm@gmail.com>
2023-03-31 20:14:20 +0700

    Add code-formatters

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
 
-let server = app.listen(port, ()=>{
-    console.log(`🚀 Listening on http://localhost:${port}`);
-}); 
+let server = app.listen(port, () => {
+  console.log(`🚀 Listening on http://localhost:${port}`);
+});
