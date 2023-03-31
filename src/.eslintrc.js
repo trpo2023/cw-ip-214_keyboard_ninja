45df199dc74099ccdcf9caa2644866c615f64ac9
Daniil Parkov <56759438+wnmz@users.noreply.github.com>
2023-03-31 20:21:19 +0700

    Create first workflow

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": ["standard","prettier"],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {        
-    }
-}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["standard", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
