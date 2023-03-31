5a5be92c816371d400781c67d35feb20a0161809
Daniil Parkov <damnwnm@gmail.com>
2023-03-31 09:33:44 +0700

    Create server, #2

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + "/public"));
+
+let server = app.listen(port, ()=>{
+    console.log(`🚀 Listening on http://localhost:${port}`);
+}); 
