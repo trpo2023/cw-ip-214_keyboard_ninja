8bdbd73c946b77a7a6c6bf8b4b7c12bf6750b50a
Vannelleg17 <inelyubin2004@mail.ru>
2023-05-04 21:30:22 +0700

    Fix codestyle, #5

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -1,18 +1,18 @@
-const select = document.getElementById('vocab_select');
+const select = document.getElementById("vocab_select");
 const main_element_id = "main_text";
 const game = new KeyboardNinja(main_element_id);
 const max_word_count = 100;
 
 set_vocabs();
 
-select.addEventListener('change', async (event) => {
-    const selected_value = event.target.value;
-    const length_input = document.getElementById('text_length');
-    let length = length_input.value;
-    
-    if(length < 10) length = 10;
-    if(length > 100) length = 100;
+select.addEventListener("change", async (event) => {
+  const selected_value = event.target.value;
+  const length_input = document.getElementById("text_length");
+  let length = length_input.value;
 
-    const text = await get_text_by_vocab_name(selected_value, length);
-    game.set_text(text);
+  if (length < 10) length = 10;
+  if (length > 100) length = 100;
+
+  const text = await get_text_by_vocab_name(selected_value, length);
+  game.set_text(text);
 });
--- src/public/scripts/vocab.js
+++ src/public/scripts/vocab.js
@@ -1,28 +1,28 @@
 async function set_vocabs() {
-    const response = await fetch('http://localhost:3000/api/get_vocabs');
-    const data = await response.json();
-    const vocabs = Array.prototype.concat.apply([], Object.values(data));
+  const response = await fetch("http://localhost:3000/api/get_vocabs");
+  const data = await response.json();
+  const vocabs = Array.prototype.concat.apply([], Object.values(data));
 
-    const select = document.getElementById('vocab_select');
-    for (let i = 0; i < vocabs.length; i++) {
-        const option = document.createElement('option');
-        option.innerText = vocabs[i];
-        select.appendChild(option);
-    }
+  const select = document.getElementById("vocab_select");
+  for (let i = 0; i < vocabs.length; i++) {
+    const option = document.createElement("option");
+    option.innerText = vocabs[i];
+    select.appendChild(option);
+  }
 }
 
 async function get_text_by_vocab_name(vocab_name, length) {
-    const response = await fetch(`http://localhost:3000/api/get_text?vocabulary=${vocab_name}`);
-    const data = await response.json();
-    const words = data['words'];
+  const response = await fetch(`http://localhost:3000/api/get_text?vocabulary=${vocab_name}`);
+  const data = await response.json();
+  const words = data["words"];
 
-    let text = "";
-    for (let i = 0; i < length; i++) {
-        let rnd = Math.round(Math.random() * words.length);
-        console.log(rnd);
-        let word = words[rnd];
-        text += word + (i < (length - 1) ? ' ' : '');
-    }
+  let text = "";
+  for (let i = 0; i < length; i++) {
+    let rnd = Math.round(Math.random() * words.length);
+    console.log(rnd);
+    let word = words[rnd];
+    text += word + (i < length - 1 ? " " : "");
+  }
 
-    return text;
+  return text;
 }
